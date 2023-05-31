a276da6225ac496b3d7e05bda6c14e2bc8b2e5f4
amandeepheliverse <amandeep@heliverse.com>
2023-05-31 19:15:54 +0530

    aadd cors policy

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
+app.use(cors())
 app.use(express.json());
 app.use(require("./routes/auth"));
 app.use(require("./routes/contactPage"));
